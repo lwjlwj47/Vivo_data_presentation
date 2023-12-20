5c2e86e9575cd98695240d970d9e1ed55a42e8eb
lwjlwj47 <82098702+lwjlwj47@users.noreply.github.com>
2023-12-20 10:55:39 +0800

    Add files via upload

---
+++ vivo_data_presentation_front/src/components/common/echarts-afterSales-machineType-high.vue
@@ -0,0 +1,58 @@
+<script setup>
+import {ref} from "vue";
+import * as echarts from 'echarts'
+import VChart from "vue-echarts";
+import axios from "axios";
+const option = ref({
+  title: {
+    text: '不同手机售后反馈数量',
+    subtext: '高端',
+    left: 'center'
+  },
+  tooltip: {
+    trigger: 'item'
+  },
+  legend: {
+    orient: 'vertical',
+    left: 'left'
+  },
+  series: [
+    {
+      name: 'Access From',
+      type: 'pie',
+      radius: '50%',
+      data: [],
+      emphasis: {
+        itemStyle: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 0, 0, 0.5)'
+        }
+      }
+    }
+  ]
+})
+axios.get('http://localhost:8080/aftersalesfeedbacks/high/length/Type')
+    .then(result=>{
+      for( name in result.data.data ){
+          option.value.series[0].data.push({"name":name,"value":result.data.data[name]})
+      }
+      console.log(option)
+
+
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+
+</script>
+
+<template>
+  <div style="height: 650px;width: 700px">
+    <v-chart class="chart" :option="option" :autoresize="true"/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>

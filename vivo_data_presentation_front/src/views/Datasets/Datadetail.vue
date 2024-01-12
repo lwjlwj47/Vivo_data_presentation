f5175185b3057b32c92af70afa6d0fe31844d4d7
president <553662886@qq.com>
2024-01-12 19:40:32 +0800

    Changes

---
+++ vivo_data_presentation_front/src/views/Datasets/Datadetail.vue
@@ -0,0 +1,58 @@
+<script setup>
+import {useRoute,useRouter} from "vue-router";
+const router = useRouter()
+const route  = useRoute()
+import {defineComponent} from 'vue'
+import { ref } from 'vue'
+import Modelintroduction from "@/components/model/modelintroduction.vue";
+const activeName = ref('first')
+const model= route.query
+console.log(JSON.parse(model.arguments))
+</script>
+
+<template>
+  <el-card class="vel_card_override">
+    <div class="top">
+      <div class="top-describe">
+        <el-text class="top-model">{{model.name}}</el-text>
+        <el-text>{{model.description}}</el-text>
+      </div>
+    </div>
+    <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+      <el-tab-pane label="模型介绍" name="first"><modelintroduction :propDate="JSON.parse(model.arguments)"></modelintroduction></el-tab-pane>
+      <el-tab-pane label="代码" name="second">这里是模型代码<br>展示模型的代码</el-tab-pane>
+      <el-tab-pane label="在线测试" name="third">这里是模型在线测试<br>在此模块中，我们可以选择我们想要的数据集输入至后端，获取后端返回结果</el-tab-pane>
+      <el-tab-pane label="在线持续训练" name="fourth">这里是模型持续训练<br>在次模块中，我们可以输入最新的训练集数据对模型进行进一步训练</el-tab-pane>
+      <el-tab-pane label="讨论" name="fifth">这里是模型相关讨论</el-tab-pane>
+    </el-tabs>
+  </el-card>
+</template>
+
+<style scoped>
+.vel_card_override{
+  margin: 20px 20px 0px 20px;
+  height: calc(100vh - 90px - 20px - 20px - 2px);
+}
+.top{
+  display: flex;
+  justify-content: space-between;
+  padding: 30px 150px 30px 150px ;
+  background-color: #ffffff;
+}
+.top-describe{
+  display: flex;
+  justify-content: center;
+  flex-direction:column;
+}
+.top-model{
+  font-size: 40px;
+  font-family: 黑体;
+  font-weight: bold;
+  color: #1b1f23;
+  margin-bottom: 10px;
+}
+.demo-tabs{
+  margin:30px 150px 30px 150px ;
+}
+
+</style>
---
+++ vivo_data_presentation_front/src/views/Datasets/Dataset.vue
@@ -0,0 +1,146 @@
+<script setup>
+import {useRouter} from "vue-router";
+import models from "../../../docs/data/dataset.json"
+import {Search} from "@element-plus/icons-vue";
+const router = useRouter()
+const url = "res/imgs/study.png"
+
+const mymodels = models.model
+
+
+const gotodetail = (model) => {
+  router.push({
+    path:"/Datasets/Datadetail",
+    query:{
+      name:model.name,
+      description:model.description,
+      arguments:JSON.stringify(model.arguments)
+    }
+  })
+}
+
+function handel(url) {
+  window.open(url,'_blank')
+}
+</script>
+
+<template>
+  <el-card class="vel_card_override">
+    <div class="content">
+      <div class="top">
+        <div class="top-describe">
+          <el-text class="top-model">数据集</el-text>
+          <el-text>OriginOS度量评价使用的数据集</el-text>
+        </div>
+        <el-image :src="url" style="width: 250px;height: 250px"> </el-image>
+      </div>
+      <div>
+        <el-input
+            v-model="input2"
+            class="search"
+            placeholder="search model"
+            :prefix-icon="Search"
+        />
+      </div>
+      <div class="trend">
+        <div class="trend-icon">
+          <el-icon  size="25px" color="#1b1f23">
+            <Star/>
+          </el-icon>
+          <div class="trend-word">
+            Trending Datasets
+          </div>
+        </div>
+        <el-button size="large" round text class="all-button">全部</el-button>
+      </div>
+      <el-row class="box" :gutter="24">
+        <el-col :span="6" v-for="model in mymodels">
+          <el-card class="box-card" shadow="hover" @click="handel(model.url)">
+            <template #header>
+              <div class="card-header">
+                <span>{{model.name}}</span>
+              </div>
+            </template>
+            <div>
+              <span>{{model.description}}</span>
+            </div>
+            <template #footer>
+              <div>Footer content
+              </div>
+            </template>
+          </el-card>
+        </el-col>
+      </el-row>
+    </div>
+  </el-card>
+</template>
+
+<style scoped>
+.vel_card_override{
+  margin: 20px 20px 0px 20px;
+  padding:0px 48px 0px 48px;
+  height: calc(100vh - 90px - 20px - 20px - 2px);
+}
+.content{
+  padding: 30px 150px 30px 150px;
+}
+
+.top{
+  display: flex;
+  justify-content: space-between;
+  background-color: #ffffff;
+}
+.top-describe{
+  display: flex;
+  justify-content: center;
+  flex-direction:column;
+}
+.search , .trend{
+  height: 48px;
+  margin:20px 0px 20px 0px;
+  color: #1b1f23;
+}
+.trend{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.trend-icon{
+  display: flex;
+  flex-direction: row;
+}
+.trend-word{
+  align-items: center;
+  margin-left: 20px;
+  font-size: 20px;
+  font-weight: bold;
+  line-height: 27px;
+  font-family: sans-serif;
+}
+.all-button{
+  font-family: 黑体;
+  font-size: 18px;
+  font-weight: bold;
+}
+:deep(.el-input__wrapper){
+  border-radius: 20px !important;
+}
+.top-model{
+  font-size: 40px;
+  font-family: 黑体;
+  font-weight: bold;
+  color: #1b1f23;
+  margin-bottom: 10px;
+}
+.box{
+}
+.box-card{
+  height: 250px;
+  border-radius:20px;
+}
+.card-header{
+  font-family: "Adobe 黑体 Std R";
+  font-size: 20px;
+  font-weight: bold;
+}
+</style>
